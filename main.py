e8f9df8900ad4d40e89a1257669f4a89724d238a
TheGreatChaosCoder <35439715+TheGreatChaosCoder@users.noreply.github.com>
2020-08-25 20:51:55 -0500

    deleted unnecessary comments

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import RPi.GPIO as GPIO
+import Keypad
+ROWS = 4    
+COLS = 4 
+keys =  [   '1','2','3','A',  
+            '4','5','6','B',
+            '7','8','9','C',
+            '*','0','#','D'     ]
+rowsPins = [12,16,18,22]        #connect to the row pinouts of the keypad
+colsPins = [19,15,13,11]        #connect to the column pinouts of the keypad
 
+def loop():
+    keypad = Keypad.Keypad(keys,rowsPins,colsPins,ROWS,COLS)  
+    keypad.setDebounceTime(50)     
+    while(True):
+        key = keypad.getKey()   
+        if(key != keypad.NULL):  
+            print ("You Pressed Key : %c "%(key))
+            
+if __name__ == '__main__':     #Program start from here
+    print ("Program is starting ... ")
+    try:
+        loop()
+    except KeyboardInterrupt:  #When 'Ctrl+C' is pressed, exit the program. 
+        GPIO.cleanup()
